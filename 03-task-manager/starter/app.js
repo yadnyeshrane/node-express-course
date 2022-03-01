c0e65196e3e099aff50ae9b3a41bb807dd556bce
Yadnyesh <yadnyeshrane1998@gmail.com>
2022-03-01 15:15:16 +0530

    Api Added

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,39 @@
-console.log('Task Manager App')
+
+const express=require('express');
+const connectDb = require('./db/connect');
+const app=express();
+const task=require('./routes/task');
+const notFound=require('./middleware/notfound');
+const errorHandler = require('./middleware/error');
+
+require('dotenv').config();
+const port=3000;
+
+
+//middleare
+app.use(express.static('./public'))
+app.use(express.json())
+//routes
+// app.get('/hello',(req,res)=>{
+//     res.send('Tak Manager')
+// })
+
+app.use('/api/v1/task',task)
+app.use(notFound);
+app.use(errorHandler);
+
+//if the db is connected then only go to server
+const start=async()=>{
+    try{
+await connectDb(process.env.MONGO_URI)
+app.listen(port,()=>{
+    console.log('server is listening')
+})
+
+    }
+    catch(error)
+    {
+console.log(error)
+    }
+}
+start()
